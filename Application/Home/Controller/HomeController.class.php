ded85ecf6d53645c0ffb23c2a534ac7ca2ff2e10
yangweijie <917647288@qq.com>
2014-02-08 17:53:48 +0800

    完成rss前10篇文章显示
    统计显示有问题，缩略内容截取有问题

--- Application/Home/Controller/HomeController.class.php
+++ Application/Home/Controller/HomeController.class.php
@@ -9,6 +9,7 @@
 
 namespace Home\Controller;
 use Think\Controller;
+use OT\Feed;
 
 /**
  * 前台公共控制器
@@ -18,14 +19,21 @@ class HomeController extends Controller {
 
 	/* 空操作，用于输出404页面 */
 	public function _empty(){
-		if(CONTROLLER_NAME == 'search'){
-			$Index = new IndexController();
-			// echo ACTION_NAME;die;
-			$_GET['kw'] = ACTION_NAME;
-			$Index->search();
-			api('Home/Index/search',array('kw'=>ACTION_NAME));
-		}else{
-			$this->error('对不起，你找的页面不存在');
+		switch (CONTROLLER_NAME) {
+			case 'search':
+				$Index = new IndexController();
+				$_GET['kw'] = ACTION_NAME;
+				$Index->search();
+				break;
+			case 'feed':
+				$count = D('Addons://Comment/comment')->group('cid')->getField('cid','count(*) as num');
+				dump($count);die;
+				$type = I('get.type');
+				$this->feed($type);
+				break;
+			default:
+				$this->error('对不起，你找的页面不存在');
+				break;
 		}
 
 	}
@@ -55,7 +63,7 @@ class HomeController extends Controller {
 		$list_row = I('r')? I('r') : 10;
 		$map = array();
 
-		$cate_id = I('get.cate_id');
+		$cate_id = I('get.cate_id', 0);
 		if($cate_id)
 			$map['category_id'] = $cate_id;
 		$cate_id = $cate_id? $cate_id : NULL;
@@ -77,6 +85,79 @@ class HomeController extends Controller {
 		$p = $page->show();
 		$this->assign('_page', $p? $p: '');
 		$this->assign('list', $list);
+		return $list;
 	}
+
+	//RSS
+    public function feed($type ='RSS2'){
+        $this->_currentFeedUrl = '';
+
+        /** 判断聚合类型 */
+        switch ($type) {
+            case 'rss':
+                /** 如果是RSS1标准 */
+                $this->_feedType = Feed::RSS1;
+                $this->_currentFeedUrl = $this->options->feedRssUrl;
+                $this->_feedContentType = 'application/rdf+xml';
+                break;
+            case 'atom':
+                /** 如果是ATOM标准 */
+                $this->_feedType = Feed::ATOM1;
+                $this->_currentFeedUrl = $this->options->feedAtomUrl;
+                $this->_feedContentType = 'application/atom+xml';
+                break;
+            default:
+                $this->_feedType = Feed::RSS2;
+                $this->_currentFeedUrl = $this->options->feedUrl;
+                $this->_feedContentType = 'application/rss+xml';
+                break;
+        }
+
+        $feedQuery = 'article';
+
+        $this->_feed = new Feed(ONETHINK_VERSION, $this->_feedType, 'UTF-8', 'zh-CN');
+
+        $list_rows = 10;
+
+        $this->_feed->setSubTitle(C('WEB_SITE_DESCRIPTION'));
+        $this->_feed->setFeedUrl($this->_currentFeedUrl);
+
+        $this->_feed->setBaseUrl(U('/', '', false, true));
+
+
+        $this->_feed->setTitle(C('WEB_SITE_TITLE') .'-文章');
+
+        $list = $this->lists(1);
+
+        foreach ($list as $key => $value) {
+
+            $feedUrl = '';
+            if (Feed::RSS2 == $this->_feedType) {
+                $feedUrl = U('/feed', '', false, true);
+            } else if (Feed::RSS1 == $this->_feedType) {
+                $feedUrl = U('/feed/rss', '', false, true);
+            } else if (Feed::ATOM1 == $this->_feedType) {
+                $feedUrl = U('/feed/atom', '', false, true);
+            }
+            $count = D('Addons://Comment/comment')->group('cid')->getField('cid','count(*) as num');
+
+            $this->_feed->addItem(array(
+                'title'     =>  $value['title'],
+                'content'   =>  C('feedFullText') ? $value['content'] : ($value['description']?$value['description']:msubstr($value['content'], 0 ,200, "utf-8", false)),
+                'date'      =>  $value['create_time'],
+                'link'      =>  U('archive/'.$value['id'], '', false, true),
+                'author'    =>  get_username($value['uid']),
+                'excerpt'   =>  $value['description'],
+                'comments'  =>  (int)$count[$value['id']],
+                'commentsFeedUrl' => $feedUrl,
+                'suffix'    =>  NULL
+            ));
+        }
+
+        header('Content-Type: ' . $this->_feedContentType . '; charset=utf-8', true);
+        echo $this->_feed->__toString();
+        exit;
+
+    }
 
 }
