3b77e38c8a2286efbbc9b8482d335365373de148
yangweijie <917647288@qq.com>
2014-02-18 23:05:26 +0800

    修改功能 完善书籍插件的默认图片显示和加数据

--- Addons/BookShell/function.php
+++ Addons/BookShell/function.php
@@ -4,6 +4,8 @@ if(!function_exists('preview_pic')){
 
 	function preview_pic($cover_id, $height=50){
 	    $src = __ROOT__ . get_cover($cover_id,'path');
+	    if($src == __ROOT__)
+	    	$src = ltrim(ONETHINK_ADDON_PATH.'Bookshell/static/images/cover.jpg','.');
 	    return <<<str
 <div class="upload-img-box">
 	<div class="upload-pre-item">
